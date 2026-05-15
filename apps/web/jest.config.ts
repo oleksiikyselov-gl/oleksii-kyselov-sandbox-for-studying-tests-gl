5a80c5db756c07f55105d5f54544ef62ebd07db9
Oleksii Kyselov <oleksii.kyselov@globallogic.com>
2026-05-15 20:01:45 +0300

    feat: Add "test:coverage" script and some config changes..

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -1,10 +1,26 @@
-import config from '../../packages/jest-preset/jest.config';
-import type { Config } from 'jest';
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require('path');
 
-const jestConfig: Config = {
-  ...config,
-  rootDir: '.',
-  setupFilesAfterEnv: ['./src/jest.setup.js'],
+export default {
+  testEnvironment: 'jsdom',
+  displayName: 'web',
+  collectCoverage: true,
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      {
+        configFile: '../../.babelrc.js',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '\\.module\\.css$': path.resolve(__dirname, '../../jest.styleMock.js'),
+    '\\.css$': path.resolve(__dirname, '../../jest.styleMock.js'),
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/',
+  coverageReporters: ['lcov'],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.spec.{ts,tsx}', '!**/*.stories.{ts,tsx}'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
 };
-
-export default jestConfig;
--- libs/services/authorization/src/user-registration/user-registration.ts
+++ libs/services/authorization/src/user-registration/user-registration.ts
@@ -1,6 +1,4 @@
 export const registerUser = async (name: string): Promise<string> => {
-  console.log('name to register:', name);
-
   return new Promise((resolve) => {
     setTimeout(() => resolve('Success!'), 500);
   });
